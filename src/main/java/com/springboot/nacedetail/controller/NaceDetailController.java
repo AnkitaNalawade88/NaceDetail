12483a48c13493ccdfd6603b5d0c6ea53e79d940
unknown <ankita.nalawade@gmail.com>
2023-09-19 14:57:27 +0100

    swagger-ui.html

--- src/main/java/com/springboot/nacedetail/controller/NaceDetailController.java
+++ src/main/java/com/springboot/nacedetail/controller/NaceDetailController.java
@@ -2,6 +2,8 @@ package com.springboot.nacedetail.controller;
 
 import com.springboot.nacedetail.entity.NaceDetail;
 import com.springboot.nacedetail.service.NaceDetailService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Api(value = "NACE", description = "Nomenclature of Economic Activities")
 public class NaceDetailController {
 
     @Autowired
@@ -20,12 +23,14 @@ public class NaceDetailController {
 
 
     @PostMapping("/nace")
+    @ApiOperation(value = "store nace details api")
     public NaceDetail addNaceDetails(@RequestBody NaceDetail naceDetail) {
         LOGGER.info("Inside addNaceDetails of NaceDetailController");
         return naceDetailService.save(naceDetail);
     }
 
     @GetMapping("/nace")
+    @ApiOperation(value = "Fetch Nace List")
     public List<NaceDetail> getNaceList() {
         LOGGER.info("Inside getNaceList of NaceDetailsController");
         return naceDetailService.getNaceList();
--- src/main/java/com/springboot/nacedetail/swagger/SwaggerConfig.java
+++ src/main/java/com/springboot/nacedetail/swagger/SwaggerConfig.java
@@ -4,10 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -25,6 +26,28 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
                 .paths(PathSelectors.any())
                 .build();
     }
+
+   /* @Bean
+    public Docket postsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("nacedetail")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(regex("/nace.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("NACE Details")
+                .description("NACE Details Documentation Generated Using SWAGGER2")
+                .termsOfServiceUrl("https://ec.europa.eu/eurostat/ramon/nomenclatures/index.cfm?TargetUrl=LST_CLS_DLD&StrNom=NACE_REV2&StrLanguage")
+                .license("TEST")
+                .licenseUrl("https://ec.europa.eu/eurostat/ramon/nomenclatures/index.cfm?TargetUrl=LST_CLS_DLD&StrNom=NACE_REV2&StrLanguage")
+                .version("1.0")
+                .build();
+    }
+    */
+
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
